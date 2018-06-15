2a2b0ae0f682294f4fe9a876397ec41051b4ca93
Taddes Korris <taddes.korris@gmail.com>
2018-06-15 18:43:03 -0400

    defined root route, added morgan, bodyParser, etc

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 // Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 app = express();
 
+const PORT = process.env.PORT || 3000;
+
+// configure body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Log requests to console
+app.use(morgan('dev'));
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('Testing the home route')
+});
+
+
+
 // Server listen
 const server = app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
+  console.log(`Server listening on port http://localhost:${PORT}`)
 });
